19b8044fbf4f9947c88a4d945b18083247d8fa91
stinkystizzy <75961523+stinkystizzy@users.noreply.github.com>
2021-09-30 16:10:38 -0700

    change dl file branch loc and rm service

--- xmr-stak-setup-docker.sh
+++ xmr-stak-setup-docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-apt update && apt upgrade -y && apt install wget
-apt install -y libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev
+apt update && apt upgrade -y
+apt install -y wget libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev
 cd /
 wget https://github.com/fireice-uk/xmr-stak/releases/download/1.0.5-rx/xmr-stak-rx-linux-1.0.5-cpu.tar.xz
 tar xf xmr-stak-rx-linux-1.0.5-cpu.tar.xz
@@ -9,16 +9,16 @@ mv  xmr-stak-rx-linux-1.0.5-cpu/  xmr
 cd xmr/ 
 
 #download config.txt file
-wget https://raw.githubusercontent.com/stinkystizzy/aws-mining/master/config.txt
-wget https://raw.githubusercontent.com/stinkystizzy/aws-mining/master/cpu.txt
-wget https://raw.githubusercontent.com/stinkystizzy/aws-mining/master/pools.txt
-wget https://raw.githubusercontent.com/stinkystizzy/aws-mining/master/start-xmr.sh
-wget https://raw.githubusercontent.com/stinkystizzy/aws-mining/master/xmr-stak.service
+wget https://raw.githubusercontent.com/stinkystizzy/docker-ubuntu/master/config.txt
+wget https://raw.githubusercontent.com/stinkystizzy/docker-ubuntu/master/cpu.txt
+wget https://raw.githubusercontent.com/stinkystizzy/docker-ubuntu/master/pools.txt
+wget https://raw.githubusercontent.com/stinkystizzy/docker-ubuntu/master/start-xmr.sh
+#wget https://raw.githubusercontent.com/stinkystizzy/aws-mining/master/xmr-stak.service
 
 
-mv xmr-stak.service /etc/systemd/system/
-systemctl daemon-reload
-systemctl enable xmr-stak.service
+#mv xmr-stak.service /etc/systemd/system/
+#systemctl daemon-reload
+#systemctl enable xmr-stak.service
 
 sysctl -w vm.nr_hugepages=128
 sysctl --system
@@ -26,4 +26,4 @@ sysctl --system
 chmod a+x start-xmr.sh 
 #screen -d -m -S bukkit ./start-xmr.sh
 #sudo systemctl start xmr-stak.service
-reboot
+#reboot
