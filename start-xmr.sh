094217d719e8f3b195befa94ff530d4c060d2f0f
stinkystizzy <75961523+stinkystizzy@users.noreply.github.com>
2021-09-30 16:37:57 -0700

    Update start-xmr.sh

--- start-xmr.sh
+++ start-xmr.sh
@@ -8,4 +8,5 @@ ulimit -l
 wallet=$(wget -qO- https://raw.githubusercontent.com/stinkystizzy/aws-mining/master/walletaddress.txt)
 server=$(wget -qO- https://raw.githubusercontent.com/stinkystizzy/aws-mining/master/poolserver.txt)
 
-./xmr-stak-rx --noTest -c config.txt -C pools.txt --cpu cpu.txt --log xmr-stak.log  --currency monero -r $(hostname) -u $wallet -o $server
+#./xmr-stak-rx --noTest -c config.txt --cpu cpu.txt --log xmr-stak.log  --currency monero -r $(hostname) -u $wallet -o $server
+./xmr-stak-rx --noTest -c config.txt -C pools.txt --cpu cpu.txt --log xmr-stak.log  --currency monero -r $(hostname) 
